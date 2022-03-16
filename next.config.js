9b520ca030db904e11c5286636e6af45d1d5f7a3
bflorestal <yayou7710@hotmail.fr>
2022-03-16 22:16:59 +0100

    Premier commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
+  nextConfig,
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
