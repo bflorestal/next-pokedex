80bbc455fe523a02cf75997548a79b44fe5377c4
bflorestal <yayou7710@hotmail.fr>
2026-02-28 21:34:20 +0100

    Remplacement emoji loupe par un SVG

--- components/molecules/Form/Form.tsx
+++ components/molecules/Form/Form.tsx
@@ -17,11 +17,21 @@ export default function Form({ handleChange, value }: FormProps) {
         onChange={handleChange}
         placeholder="Rechercher..."
       />
-      <input
-        type="submit"
-        className={styles.search__btn}
-        value="🔎"
-      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={styles.search__icon}
+      >
+        <path d="m21 21-4.34-4.34" />
+        <circle cx="11" cy="11" r="8" />
+      </svg>
     </div>
   );
 }
